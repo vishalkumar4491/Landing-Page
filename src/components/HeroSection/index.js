db924cb30e4d381e9ae5178391f566b22abbe736
vishalkumar44 <ddaa44456@gmail.com>
2021-06-21 23:13:28 +0530

    Complete Landing Page

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { Button } from '../ButtonElements'
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer id='home'>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Buy And Sell Your Book</HeroH1>
+                <HeroP>
+                    Sign Up today to Sell Your Old Books
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary = 'true' dark = 'true'>Get Started {hover ? <ArrowForward /> : <ArrowRight />}</Button>
+                </HeroBtnWrapper>
+
+
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
---
+++ src/components/Signin/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormInput, FormButton, Text } from './SigninElements'
+
+const SignIn = () => {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to="/">Dolla</Icon>
+                    <FormContent>
+                        <Form action="#">
+                            <FormH1>Sign In to your account</FormH1>
+                            <FormLabel htmlFor="for">Email</FormLabel>
+                            <FormInput type="email" requied />
+                            <FormLabel htmlFor="for">Password</FormLabel>
+                            <FormInput type="password" requied />
+                            <FormButton type="submit">Continue</FormButton>
+                            <Text>Forgot Password</Text>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    )
+}
+
+export default SignIn
